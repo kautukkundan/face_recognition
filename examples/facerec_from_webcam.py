5dec20870d11ed5bcae06eccc65aa2ef0ae46409
Chetan Madan <chetanmadan24@gmail.com>
2018-09-28 19:33:27 +0530

    add known_faces

--- examples/facerec_from_webcam.py
+++ examples/facerec_from_webcam.py
@@ -12,7 +12,7 @@ import cv2
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("Chetan.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
@@ -25,7 +25,7 @@ known_face_encodings = [
     biden_face_encoding
 ]
 known_face_names = [
-    "Barack Obama",
+    "Chetan Madan",
     "Joe Biden"
 ]
 
